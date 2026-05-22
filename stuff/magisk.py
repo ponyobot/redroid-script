84fea45f6fdebfcd0e44edd88d9a8b5a9177abd0
ponyobot <ponyobot0000@gmail.com>
2026-05-22 16:10:54 +0900

    first commit

--- stuff/magisk.py
+++ stuff/magisk.py
@@ -31,27 +31,25 @@ on post-fs-data
     exec u:r:su:s0 root root -- {MAGISKSYSTEMDIR}/magiskpolicy --live --magisk
     exec u:r:magisk:s0 root root -- {MAGISKSYSTEMDIR}/magiskpolicy --live --magisk
     exec u:r:update_engine:s0 root root -- {MAGISKSYSTEMDIR}/magiskpolicy --live --magisk
-    exec u:r:su:s0 root root -- {MAGISKSYSTEMDIR}/{magisk_name} --auto-selinux --setup-sbin {MAGISKSYSTEMDIR} {MAGISKTMP}
-    exec u:r:su:s0 root root -- {MAGISKTMP}/magisk --auto-selinux --post-fs-data
+    exec u:r:su:s0 root root -- {MAGISKSYSTEMDIR}/magisk --daemon
+    exec u:r:su:s0 root root -- {MAGISKSYSTEMDIR}/magisk --post-fs-data
 on nonencrypted
-    exec u:r:su:s0 root root -- {MAGISKTMP}/magisk --auto-selinux --service
+    exec u:r:su:s0 root root -- {MAGISKSYSTEMDIR}/magisk --service
 on property:vold.decrypt=trigger_restart_framework
-    exec u:r:su:s0 root root -- {MAGISKTMP}/magisk --auto-selinux --service
+    exec u:r:su:s0 root root -- {MAGISKSYSTEMDIR}/magisk --service
 on property:sys.boot_completed=1
     mkdir /data/adb/magisk 755
-    exec u:r:su:s0 root root -- {MAGISKTMP}/magisk --auto-selinux --boot-complete
+    exec u:r:su:s0 root root -- {MAGISKSYSTEMDIR}/magisk --boot-complete
     exec -- /system/bin/sh -c "if [ ! -e /data/data/com.topjohnwu.magisk ] ; then pm install /system/etc/init/magisk/magisk.apk ; fi"
-   
 on property:init.svc.zygote=restarting
-    exec u:r:su:s0 root root -- {MAGISKTMP}/magisk --auto-selinux --zygote-restart
-   
+    exec u:r:su:s0 root root -- {MAGISKSYSTEMDIR}/magisk --zygote-restart
 on property:init.svc.zygote=stopped
-    exec u:r:su:s0 root root -- {MAGISKTMP}/magisk --auto-selinux --zygote-restart
-    """.format(MAGISKSYSTEMDIR="/system/etc/init/magisk", MAGISKTMP="/sbin", magisk_name="magisk")
+    exec u:r:su:s0 root root -- {MAGISKSYSTEMDIR}/magisk --zygote-restart
+    """.format(MAGISKSYSTEMDIR="/system/etc/init/magisk")
 
     def download(self):
         print_color("Downloading latest Magisk now .....", bcolors.GREEN)
-        super().download()   
+        super().download()
 
     def copy(self):
         if os.path.exists(self.copy_dir):
@@ -73,20 +71,18 @@ on property:init.svc.zygote=stopped
         lib_dir = os.path.join(self.extract_to, "lib", arch_map[self.machine[0]])
         for parent, dirnames, filenames in os.walk(lib_dir):
             for filename in filenames:
-                o_path = os.path.join(lib_dir, filename)  
-                filename = re.search('lib(.*)\.so', filename)
+                o_path = os.path.join(lib_dir, filename)
+                filename = re.search(r'lib(.*)\.so', filename)
                 n_path = os.path.join(self.magisk_dir, filename.group(1))
                 shutil.copyfile(o_path, n_path)
                 run(["chmod", "+x", n_path])
-        shutil.copyfile(self.dl_file_name, os.path.join(self.magisk_dir,"magisk.apk") )
+        shutil.copyfile(self.dl_file_name, os.path.join(self.magisk_dir, "magisk.apk"))
 
-        # Updating Magisk from Magisk manager will modify bootanim.rc, 
-        # So it is necessary to backup the original bootanim.rc.
         bootanim_path = os.path.join(self.copy_dir, "system", "etc", "init", "bootanim.rc")
-        gz_filename = os.path.join(bootanim_path)+".gz"
-        with gzip.open(gz_filename,'wb') as f_gz:
+        gz_filename = os.path.join(bootanim_path) + ".gz"
+        with gzip.open(gz_filename, 'wb') as f_gz:
             f_gz.write(self.oringinal_bootanim.encode('utf-8'))
         with open(bootanim_path, "w") as initfile:
-            initfile.write(self.oringinal_bootanim+self.bootanim_component)
+            initfile.write(self.oringinal_bootanim + self.bootanim_component)
 
         os.chmod(bootanim_path, 0o644)
